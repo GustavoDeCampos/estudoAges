6caabe2fcc8ea21743e70f304462bb0d0128f189
Gustavo Campos <guscampos2077@gmail.com>
2024-03-09 14:10:56 -0300

    otimizando

---
+++ build/estudo1.js
@@ -0,0 +1,5 @@
+"use strict";
+const username = "gustavo";
+const age = 20;
+console.log(username);
+console.log(age);
---
+++ src/arq1.ts
@@ -0,0 +1,11 @@
+class Teste{
+    canal = null;
+    curso = null;
+    constructor(canal:any, curso:any){
+        this.canal = canal;
+        this.curso = curso;
+    }   
+}
+let c1 = new Teste('Canal 1', 'Curso 1');
+console.log(c1.canal);
+console.log(c1.curso);
