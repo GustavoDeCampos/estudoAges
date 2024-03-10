6077327d409d0606c6a99e8fe168d55ed18f37ff
Gustavo Campos <guscampos2077@gmail.com>
2024-03-10 15:11:02 -0300

    aprendendo sobre objetos e um pouco de funções

--- build/objeto.js
+++ build/objeto.js
@@ -1,1 +1,14 @@
 "use strict";
+let dados = {
+    nome1: "Gustavo",
+    idade: 20,
+    stats: "Ativo",
+    info: (p) => { console.log(p); }
+};
+console.log(dados);
+console.log("Tipo: " + typeof (dados));
+console.log("Nome: " + dados.nome1);
+dados.idade = 21;
+console.log("Nova Idade: " + dados.idade);
+dados.nome1 = "junior";
+dados.info(dados.nome1 + " é um programador");
---
+++ src/objeto.ts
@@ -0,0 +1,16 @@
+let dados={ //em javascript um object pode receber propriedades e metodos, sao variaveis que podem receber qualquer tipo de dado
+    nome1: "Gustavo", //em typescrypt o sinal "=" serve para atribuir um valor a uma variavel, enquanto o sinal ":" serve para definir o tipo de uma variavel
+    idade: 20,
+    stats: "Ativo",
+    info:(p:string)=>{console.log(p)} //podemos tambem criar funções dentro de um object
+}//a vantagem de trabalhar com object é que podemos acessar suas propriedades e metodos de forma mais facil, podemos operar com os elementos do object
+
+//para trabalharmos com o object nao precismos especificar que algo é um object, o proprio typescript ja reconhece atraves da estrutura do object "{}"
+
+console.log(dados) 
+console.log("Tipo: " + typeof (dados)) //typeof é um operador que retorna o tipo de uma variavel
+console.log("Nome: " + dados.nome1) //para acessar uma propriedade de um object usamos o ponto
+dados.idade = 21 //para alterar o valor de uma propriedade de um object usamos o sinal de atribuição
+console.log("Nova Idade: " + dados.idade)
+dados.nome1 = "junior"
+dados.info(dados.nome1 + " é um programador")//aqui estamos chamando a função, como ela recebe um parametro do tipo string, ela so pode ser acessada atraves de strings, como nome1 é uma string ela vai retornar o nome do object
