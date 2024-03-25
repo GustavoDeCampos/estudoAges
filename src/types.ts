5707fbc45a9794a053c8ba1b865e31ca8b4a94c8
Gustavo Campos <guscampos2077@gmail.com>
2024-03-25 16:08:10 -0300

    comitando

--- src/types.ts
+++ src/types.ts
@@ -10,18 +10,18 @@ n = 10;
 s = "20";
 
 s = n.toString(); //aqui é feito um typecast de number para string, ou seja, o numero 10 é um numero inteiro, mas passa a ser uma string graças ao casting
-//n =Number.parseInt(s); //nao é possivel fazer uma associação direta de um string para um number, somente com casting
+n =Number.parseInt(s); //nao é possivel fazer uma associação direta de um string para um number, somente com casting
 //aqui é feito um typecast de string para number, ou seja, o numero 20 é uma string, mas passa a ser um numero inteiro graças ao casting
 
-//u = 10; // nao é possivel fazer uma associação direta de um unknown para um number, somente uma afirmação de tipo
-//n = u as number; //afirmação de tipo
-//n = <number>u; //afirmação de tipo
+u = 10; // nao é possivel fazer uma associação direta de um unknown para um number, somente uma afirmação de tipo
+n = u as number; //afirmação de tipo
+n = <number>u; //afirmação de tipo
 
-//console.log(typeof u);
-//console.log(u);
+console.log(typeof u);
+console.log(u);
 
-//console.log(typeof n);
-//console.log(n);
+console.log(typeof n);
+console.log(n);
 
 console.log(typeof s);
 console.log(s);
